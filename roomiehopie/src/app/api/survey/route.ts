a8a591789a0eecde22b8e4e91130066d42c6b47f
Jenny Zhang <95259224+jxnnyzhang@users.noreply.github.com>
2025-04-01 11:53:26 -0400

    Delete route.ts

--- roomiehopie/src/app/api/survey/route.ts
+++
@@ -1,6 +0,0 @@
-import { NextResponse } from "next/server";
-
-export async function POST(req: Request) {
-    console.log("API request received:", req.method); // ✅ Logs the request
-    return NextResponse.json({ success: true }, { status: 200 });
-  }
